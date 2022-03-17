7b6e709ffe304de87e52e718e84675059f469f6d
FLorin <florinainol@gmail.com>
2022-03-17 18:26:09 +0200

    Responsive + Redirects (login / signup)

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,13 +1,19 @@
 import "./LoginStyles.scss";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import LeftImage from "../../components/LeftImage/LeftImage";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div className="login">
-      <LeftImage />
+      <div className="login__left">
+        <LeftImage />
+      </div>
       <div className="login__form">
         <LoginForm />
+        <span className="login__signup-redirect">
+          Nu ai inca cont? <Link to="/signup">Inregistreaza-te</Link>{" "}
+        </span>
       </div>
     </div>
   );
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,14 +1,18 @@
 import "./SignUpStyles.scss";
 import LeftImage from "../../components/LeftImage/LeftImage";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
     <div className="signup">
-      <LeftImage />
+      <div className="signup__left">
+        <LeftImage />
+      </div>
       <div className=""></div>
       <div className="signup__form">
         <SignUpForm />
+        <span className="signup__login-redirect">Ai deja cont? <Link to="/login">Logheaza-te</Link> </span>
       </div>
     </div>
   );
