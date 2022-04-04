59d56fe90a874e8c844080bee9553f9ca9a5e55d
FLorin <florinainol@gmail.com>
2022-04-04 22:16:08 +0300

    Sync

--- src/components/HomePage/NavigationBar/Logo/Logo.jsx
+++ src/components/HomePage/NavigationBar/Logo/Logo.jsx
@@ -1,12 +1,7 @@
 import "./LogoStyles.scss";
 
-<<<<<<< HEAD
 import srcLogo from "../../../../assets/images/logo-blackx.svg";
 // schimbare
-=======
-import srcLogo from "../../../../assets/images/logo-left-image.svg";
-
->>>>>>> Loghin
 const Logo = () => {
   return (
     <div className="logo">
