24bf8d8ee902a9cee865c6b70898599ed9107bd8
FLorin <florinainol@gmail.com>
2022-04-05 13:27:05 +0300

    Admin Table for users

--- src/Pages/WaitingList/WaitingList.jsx
+++ src/Pages/WaitingList/WaitingList.jsx
@@ -5,76 +5,160 @@ import InnerImageZoom from "react-inner-image-zoom";
 import "react-inner-image-zoom/lib/InnerImageZoom/styles.min.css";
 // import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import Button from "@mui/material/Button";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import ThumbDownAltIcon from "@mui/icons-material/ThumbDownAlt";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 const columns = [
-  { field: "id", headerName: "ID", width: 100 },
-  // { field: "firstName", headerName: "First name", width: 170 },
-  // { field: "lastName", headerName: "Last name", width: 170 },
-  // {
-  //   field: "age",
-  //   headerName: "Age",
-  //   // type: "number",
-  //   width: 90,
-  // },
+  { field: "id", headerName: "ID" },
   {
     field: "fullName",
     headerName: "Nume Prenume",
-    // description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 250,
-    // valueGetter: (params) =>
-    //   `${params.row.firstName || ""} ${params.row.lastName || ""}`,
   },
   {
     field: "registerTime",
     headerName: "Data si ora inregistrarii",
-    // type: "number",
-    width: 250,
   },
   {
     field: "locationRegister",
     headerName: "Zona",
-    // type: "number",
-    width: 130,
   },
   {
     field: "proof",
     headerName: "Dovada",
-    // type: "number",
-    width: 130,
+  },
+  {
+    field: "action",
+    headerName: "Actiuni",
   },
 ];
 
 const rows = [
   {
-    id: 1,
+    id: nanoid(),
     fullName: "Florin Bucataru",
     registerTime: "12.06.2022 15:00",
     locationRegister: "Iasi, Copou",
     proof: "https://imgur.com/gallery/zFUN42E",
   },
-  // { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  // { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  // { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  // { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  // { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  // { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  // { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  // { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  {
+    id: nanoid(),
+    fullName: "Florin Bucataru",
+    registerTime: "12.06.2022 15:00",
+    locationRegister: "Iasi, Copou",
+    proof: "https://imgur.com/gallery/zFUN42E",
+  },
 ];
 
 const WaitingList = () => {
+  const [open, setOpen] = useState(false);
+
+  const dialogAction = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className="waiting-list">
       <div className="waiting-list__table">
-        <DataGrid
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    className="waiting-list__column-title"
+                    align="left"
+                    key={nanoid()}
+                  >
+                    {column.headerName}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  {/* <TableCell component="th" scope="row">
+                    {row.id}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell> */}
+                  <TableCell align="left" className="waiting-list__row">
+                    {row.id}
+                  </TableCell>
+                  <TableCell align="left" className="waiting-list__row">
+                    {row.fullName}
+                  </TableCell>
+                  <TableCell align="left" className="waiting-list__row">
+                    {row.registerTime}
+                  </TableCell>
+                  <TableCell align="left" className="waiting-list__row">
+                    {row.locationRegister}
+                  </TableCell>
+                  <TableCell align="left" className="waiting-list__row">
+                    <IconButton
+                      onClick={dialogAction}
+                      className="waiting-list__proof-btn"
+                      color="primary"
+                      aria-label="upload picture"
+                      component="span"
+                    >
+                      <VisibilityIcon />
+                    </IconButton>
+                  </TableCell>
+                  <TableCell
+                    align="left"
+                    className="waiting-list__row waiting-list__row--f-column"
+                  >
+                    <Button variant="outlined" startIcon={<ThumbUpAltIcon />}>
+                      Aproba
+                    </Button>
+                    <Button variant="outlined" startIcon={<ThumbDownAltIcon />}>
+                      Respinge
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        {/* <DataGrid
           rows={rows}
           columns={columns}
           //   rowsPerPageOptions={[5]}
           checkboxSelection
-        />
+        /> */}
       </div>
-      {/* <InnerImageZoom src={buletin} zoomScale={2} zoomSrc={buletin} /> */}
+      <Dialog
+        open={open}
+        onClose={dialogAction}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogContent>
+          <InnerImageZoom src={buletin} zoomScale={2} zoomSrc={buletin} />
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
