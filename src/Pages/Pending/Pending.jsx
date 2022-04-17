703912df87f6effb6799643bff1406aa12f6b647
Florin <florinainol@gmail.com>
2022-04-17 11:18:53 +0300

    Pending page

--- src/Pages/Pending/Pending.jsx
+++ src/Pages/Pending/Pending.jsx
@@ -1,6 +1,13 @@
 import "./PendingStyles.scss";
 import Lottie from "react-lottie";
 import pendingAnimation from "../../assets/lotties/pending.json";
+import { useEffect, useState } from "react";
+import { Cookies, useCookies } from "react-cookie";
+import axios from "../../assets/axios/axios";
+import { useNavigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+
+
 
 const Pending = () => {
   const lottieOptions = {
@@ -11,12 +18,47 @@ const Pending = () => {
       preserveAspectRatio: "xMidYMid slice",
     },
   };
+  const [cookies, setCookie, removeCookie] = useCookies(["token"]);
+  const [loader, setLoader] = useState(false);
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    setLoader(true);
+    const userId = jwt_decode(cookies.token).userId;
+    console.log(userId)
+    axios
+      .get(
+        `/users/${userId}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${cookies.token}`,
+          },
+        }
+      )
+      .then((response) => {
+        // handle success
+        console.log(response);
+        if (response.data.status != "IN_ASTEPTARE") navigate(-1);
+        setLoader(false);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      })
+      .then(() => {
+        // always executed
+      });
+  }, [])
+
   return (
-    <div className="pending">
-      <Lottie options={lottieOptions} height={400} width={400} />
-      <h4 className="pending__title">Cererea ta se proceseaza.</h4>
-      <h5 className="pending__subtitle">Vei primi un email atunci cand este finalizata.</h5>
-    </div>
+    !loader ? 
+      <div className="pending">
+        <Lottie options={lottieOptions} height={400} width={400} />
+        <h4 className="pending__title">Cererea ta se proceseaza.</h4>
+        <h5 className="pending__subtitle">Vei primi un email atunci cand este finalizata.</h5>
+      </div>
+    : ""
   );
 };
 
