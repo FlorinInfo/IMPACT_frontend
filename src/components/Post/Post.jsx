d87920f73df1011b908c4f2baf73c15dbb29d544
Loghin024 <loghincatalin024@gmail.com>
2022-04-27 16:26:40 +0300

    upd

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,15 +1,10 @@
 import "./PostStyles.scss";
 import { Link } from "react-router-dom";
+import { BiDownvote, BiUpvote } from "react-icons/bi";
 import profileImage from "../../assets/images/default_profile_pic1.jpg";
 import { useEffect, useState } from "react";
 
-import { BiDownvote, BiUpvote } from "react-icons/bi";
-import { VscComment } from "react-icons/vsc";
-import { MdFavorite } from "react-icons/md";
-import { MdMoreHoriz } from "react-icons/md";
-
 import MediaSlider from "./MediaSlider/MediaSlider";
-import PostOptions from "./PostOptions/PostOptions";
 
 import testImage from "../../assets/images/dwipm_18.png";
 import testImage2 from "../../assets/images/buletine-840x500.jpg";
@@ -21,52 +16,38 @@ const testMedia = [
   testImage2,
 ];
 
-const Post = () => {
+const Post = ({ article }) => {
   const [cookies, setCookie, removeCookie] = useCookies(["token"]);
-  const [showOptions, setShowOptions] = useState(false);
-  const [favorite, setFavorite] = useState("");
-  const numLikes = "54";
 
-  const handleFavorite = () => {
-    favorite ? setFavorite("") : setFavorite("favorite");
-  };
-  const Post = ({ article }) => {
-    const [cookies, setCookie, removeCookie] = useCookies(["token"]);
-
-    const handleShowOptions = () => {
-      showOptions ? setShowOptions(false) : setShowOptions(true);
-    };
-
-    return (
-      <div className="post">
-        {/* <video controls muted autoPlay>
+  return (
+    <div className="post">
+      {/* <video controls muted autoPlay>
                         <source src="https://backend.imp-act.ml/assets/videosArticles/7Oog2e1I_aOnSAr322I0U.mp4" type="video/mp4"></source>
                     </video> */}
-        {/* <div className="post__votes">
-          <BiUpvote
-            className={`post__votes-action post__votes-action--up ${
-              true == true ? "post__votes-action--active-1" : ""
-            }`}
-          />
-          <span className="post__votes-number">123</span>
-          <BiDownvote className="post__votes-action post__votes-action--down" />
-        </div>
-        <div className="post__main">
-          <div className="post__author">
-            <img src={profileImage} alt="" />
-            <Link to="#" className="post__author-name">
-              {article.author.firstName} {article.author.lastName}
-            </Link>
-          </div>
-          <span className="post__title">{article.title}</span>
-          <p className="post__description">{article.description}</p>
-          <div className="post__media">
-            <MediaSlider media={article.articleGallery} />
-          </div>
-        </div> */}
+      <div className="postvotes">
+        <BiUpvote
+          className={`postvotes-action postvotes-action--up ${
+            true == true ? "postvotes-action--active-1" : ""
+          }`}
+        />
+        <span className="postvotes-number">123</span>
+        <BiDownvote className="postvotes-action postvotes-action--down" />
       </div>
-    );
-  };
+      <div className="postmain">
+        <div className="postauthor">
+          <img src={profileImage} alt="" />
+          <Link to="#" className="postauthor-name">
+            {article.author.firstName} {article.author.lastName}
+          </Link>
+        </div>
+        <span className="posttitle">{article.title}</span>
+        <p className="postdescription">{article.description}</p>
+        <div className="post__media">
+          <MediaSlider media={article.articleGallery} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
