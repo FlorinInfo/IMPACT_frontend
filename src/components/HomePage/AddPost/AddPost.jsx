5c371a8edd523e7b4e0006ce231b2b5329b7e30c
FLorin <florinainol@gmail.com>
2022-05-05 19:36:32 +0300

    Homepage add post rank

--- src/components/HomePage/AddPost/AddPost.jsx
+++ src/components/HomePage/AddPost/AddPost.jsx
@@ -5,12 +5,24 @@ import "./AddPostStyles.scss";
 
 import ProfilePicture from "./../../../assets/images/default_profile_pic1.jpg";
 
+import { useState, useContext } from "react";
+import { ImpactStore } from "../../../store/ImpactStore";
+import rankPerform from "../../../utils/rank";
+
 const AddPost = () => {
   const navigate = useNavigate();
+  const { user, setUser } = useContext(ImpactStore);
+  const [rank, setRank] = useState(
+    user.monthlyPoints ?()=>rankPerform(user.monthlyPoints, user.zoneRole, user.admin) : {
+      type:"Cetatean",
+      color:"black",
+      image:"default.jpg"
+    }
+  );
 
   return (
     <div className="main-container section--create-post">
-      <img src={ProfilePicture} />
+      <img src={require(`../../../assets/images/ranks/${rank.image}`)} />
       <input
         className="input-default"
         id="create-post"
