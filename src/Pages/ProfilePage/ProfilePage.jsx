67a941edd21f22970b47d611e9451e8007d98dd2
FLorin <florinainol@gmail.com>
2022-05-08 17:20:49 +0300

    My profile posts

--- src/Pages/ProfilePage/ProfilePage.jsx
+++ src/Pages/ProfilePage/ProfilePage.jsx
@@ -7,24 +7,28 @@ import imgProfile from "./../../assets/images/default_profile_pic1.jpg";
 import { ImpactStore } from "../../store/ImpactStore";
 import axios from "./../../assets/axios/axios";
 import { Cookies, useCookies } from "react-cookie";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useNavigate, useLocation } from "react-router-dom";
 
 import SortPostsProfile from "../../components/SortPostsProfile/SortPostsProfile";
 import { useState } from "react";
 import rankPerform from "../../utils/rank";
+import Post from "../../components/Post/Post";
 
 const ProfilePage = () => {
   // const { user, setUser } = useContext(ImpactStore);
+  let navigate = useNavigate();
   const [cookies, setCookie, removeCookie] = useCookies(["token"]);
   const { id, filter } = useParams();
   const [user, setUser] = useState(null);
-  const [rank, setRank] = useState(
-        {
-          type: "Cetatean",
-          color: "black",
-          image: "default.jpg",
-        }
-  );
-
+  const [rank, setRank] = useState({
+    type: "Cetatean",
+    color: "black",
+    image: "default.jpg",
+  });
+  const [page, setPage] = useState(0);
+  const [limit, setLimit] = useState(1);
+  const [posts, setPosts] = useState([]);
 
   // console.log(id, "fdgdfs");
 
@@ -44,7 +48,7 @@ const ProfilePage = () => {
             ? () =>
                 rankPerform(
                   response.data.monthlyPoints,
-                  response.data.roleUser,
+                  response.data.zoneRole,
                   response.data.admin
                 )
             : {
@@ -53,7 +57,7 @@ const ProfilePage = () => {
                 image: "default.jpg",
               }
         );
-      
+
         console.log(response);
       })
       .catch((error) => {
@@ -65,28 +69,84 @@ const ProfilePage = () => {
 
   const getPosts = () => {
     axios
-      .get(`https://backend.imp-act.ml/articles?userId=&admin=&recent=&inProgress=&completed=&best=&offset&limit&cursor=&favorites=&upvoted&downvoted=&timespan=&q=`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${cookies.token}`,
-        },
-      })
-      .then((response) => {
+      .get(
+        `/articles?${filter ? filter + "=true" : `userId=${id}`}&offset=${
+          page * 10
+        }&limit=10&cursor=`,
+        {
+          headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${cookies.token}`,
+          },
+        }
+      )
+      .then(async (response) => {
         // handle success
-        console.log(response);
+        console.log("aaaaaaaaaaaaaaaaaaaaA", response);
+        if (response.data.errors) navigate("/");
 
+        // console.log(response);
+
+        // Removing dublicates
+        // verifyArray = verifyArray.filter(
+        //   (value, index, self) =>
+        //     index ===
+        //     self.findIndex(
+        //       (t) => t.id === value.id
+        //     )
+        // );
+        //
+        setLimit(response.data.limit);
+        setPosts([...posts, ...response.data.articles]);
       })
       .catch((error) => {
         // handle error
         console.log(error);
       })
-      .then(() => {});
+      .then(() => {
+        // always executed
+      });
   };
 
   useEffect(() => {
     fetchData();
+    getPosts();
   }, []);
-  // const name
+
+  useEffect(() => {
+    getPosts();
+    // getTop();
+  }, [page]);
+
+  const updateArticle = (articleId) => {
+    axios
+      .get(`/articles/${articleId}`, {
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${cookies.token}`,
+        },
+      })
+      .then((response) => {
+        // handle success
+        let index = posts.findIndex((el) => el.id == articleId);
+        let newPosts = [...posts];
+        newPosts[index] = response.data;
+        setPosts(newPosts);
+        // getTop();
+        console.log(response);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      })
+      .then(() => {
+        // always executed
+      });
+  };
+
+  const deleteArticle = (articleId) => {
+    setPosts(posts.filter((p) => p.id != articleId));
+  };
 
   return (
     <div className="profile-page">
@@ -94,7 +154,10 @@ const ProfilePage = () => {
         <div className="profile-page__container">
           <div className="profile-page__left">
             <div className="profile-component">
-              <img src={require(`../../assets/images/ranks/${rank.image}`)} className="profile-component__image" />
+              <img
+                src={require(`../../assets/images/ranks/${rank.image}`)}
+                className="profile-component__image"
+              />
               <div className="profile-component__data">
                 <div className="profile-component_characteristic">
                   <span className="profile-component__data__name">Nume:</span>
@@ -112,17 +175,57 @@ const ProfilePage = () => {
                 </div>
                 <div className="profile-component_characteristic">
                   <span className="profile-component__data__name">Rank:</span>
-                  <span className="profile-component__data__value"> {rank.type}</span>
+                  <span className="profile-component__data__value">
+                    {" "}
+                    {rank.type}
+                  </span>
                 </div>
                 <div className="profile-component_characteristic">
                   <span className="profile-component__data__name">
                     Puncte Rank:
                   </span>
-                  <span className="profile-component__data__value"> {user.monthlyPoints}</span>
+                  <span className="profile-component__data__value">
+                    {" "}
+                    {user.monthlyPoints}
+                  </span>
                 </div>
               </div>
             </div>
             <SortPostsProfile />
+            {/* {posts.length} */}
+            {posts.length != 0 ? (
+              <InfiniteScroll
+                dataLength={posts.length} //This is important field to render the next data
+                next={() => setPage(page + 1)}
+                hasMore={true}
+                loader={
+                  posts.length == limit ? (
+                    <h4 className="scroll-text">Ai vazut toate postarile</h4>
+                  ) : (
+                    <h4 className="scroll-text">Loading...</h4>
+                  )
+                }
+              >
+                <div className="homepage__posts">
+                  {posts.map((article) => (
+                    <div key={article.id} className="homepage__post">
+                      {/* Judet : {article.countyId ? article.countyId : ""}
+                    <br />
+                    Oras/comuna : {article.villageId ? article.villageId : ""}
+                    <br />
+                    Localitate : {article.localityId ? article.localityId : ""} */}
+                      <Post
+                        deleteArticle={deleteArticle}
+                        updateArticle={updateArticle}
+                        article={article}
+                      />{" "}
+                    </div>
+                  ))}
+                </div>
+              </InfiniteScroll>
+            ) : (
+              <h4 className="scroll-text">Loading...</h4>
+            )}
           </div>
           <div className="profile-page__right"></div>
         </div>
