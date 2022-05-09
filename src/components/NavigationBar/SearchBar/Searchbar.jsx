fc89f8d91f6bc744fa853689037dc621dd088b79
Loghin024 <loghincatalin024@gmail.com>
2022-05-09 18:04:52 +0300

    update

--- src/components/NavigationBar/SearchBar/Searchbar.jsx
+++ src/components/NavigationBar/SearchBar/Searchbar.jsx
@@ -1,15 +1,19 @@
 import "./SearchBarStyles.scss";
 
 import SearchIcon from "@material-ui/icons/Search";
+import SearchBarResults from "../SearchBarResults/SearchBarResults";
 
 const SearchBar = () => {
   return (
-    <div className="searchbar">
-      <label htmlFor="searchbar">
-        <SearchIcon />
-      </label>
-      <input className="input-default" id="searchbar" placeholder="Caută" />
-    </div>
+    <>
+      <div className="searchbar">
+        <label htmlFor="searchbar">
+          <SearchIcon />
+        </label>
+        <input className="input-default" id="searchbar" placeholder="Caută" />
+        <SearchBarResults />
+      </div>
+    </>
   );
 };
 
--- src/components/NavigationBar/SearchBarResults/SearchBarResults.jsx
+++ src/components/NavigationBar/SearchBarResults/SearchBarResults.jsx
@@ -1,29 +1,66 @@
-import react from "react";
+import react, { useEffect } from "react";
+import { Cookies, useCookies } from "react-cookie";
+
+import axios from "./../../../assets/axios/axios";
 
 import "./SearchBarResultsStyles.scss";
 
 const SearchBarResults = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(["token"]);
+  let searchBarInput = "cillum";
+
+  const fetchData = () => {
+    axios
+      .get(`/articles?q=${searchBarInput}&offset=0&limit=3`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${cookies.token}`,
+        },
+      })
+      .then((response) => {
+        // handle success
+        console.log(response);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      })
+      .then(() => {
+        // always executed
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="results">
-      <div className="results-item">
-        <span>title</span>
-        <span>text</span>
-        <span>
-          <img />
+      <div className="results__item">
+        <span className="results__item__title">title</span>
+        <span className="results__item__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          id fermentum quam. Duis in luctus dui. Phasellus sit amet placerat
+          est, et mattis nulla. Suspendisse ac tempus magna. Nam venenatis lorem
+          justo
         </span>
       </div>
-      <div className="results-item">
-        <span>title</span>
-        <span>text</span>
-        <span>
-          <img />
+      <div className="results__item">
+        <span className="results__item__title">title</span>
+        <span className="results__item__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          id fermentum quam. Duis in luctus dui. Phasellus sit amet placerat
+          est, et mattis nulla. Suspendisse ac tempus magna. Nam venenatis lorem
+          justo
         </span>
       </div>
-      <div className="results-item">
-        <span>title</span>
-        <span>text</span>
-        <span>
-          <img />
+      <div className="results__item">
+        <span className="results__item__title">title</span>
+        <span className="results__item__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          id fermentum quam. Duis in luctus dui. Phasellus sit amet placerat
+          est, et mattis nulla. Suspendisse ac tempus magna. Nam venenatis lorem
+          justo
         </span>
       </div>
     </div>
